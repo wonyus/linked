d2aa5df6a41cbb23454a16b6aea31bc2818d0ae1
WonYus <lucy_oliv@outlook.com>
2024-05-04 01:10:07 +0700

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,53 @@
 pipeline {
     agent any
-
+    
+    environment {
+        DOCKER_IMAGE_NAME = 'wonyus/linked'
+        DOCKER_REGISTRY_CREDENTIALS = 'docker-credential'
+    }
+    
     stages {
-        stage('Build') {
+        stage('Checkout') {
             steps {
-                echo 'Building..'
+                checkout scm
             }
         }
-        stage('Test') {
+        
+        stage('Build Docker Image') {
             steps {
-                echo 'Testing..'
+                script {
+                    docker.build("${DOCKER_IMAGE_NAME}:${env.BUILD_NUMBER}")
+                    docker.build("${DOCKER_IMAGE_NAME}:latest")
+                }
             }
         }
+        
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    docker.withRegistry('https://hub.docker.com/r/wonyus/linked', "${DOCKER_REGISTRY_CREDENTIALS}") {
+                        docker.image("${DOCKER_IMAGE_NAME}:${env.BUILD_NUMBER}").push()
+                        docker.image("${DOCKER_IMAGE_NAME}:latest").push()
+                    }
+                }
+            }
+        }
+        
         stage('Deploy') {
             steps {
-                echo 'Deploying....'
+                // Add your deployment steps here
+                // For example:
+                sh 'pwd'
             }
+        }
+    }
+    
+    post {
+        success {
+            echo 'Build and deployment successful!'
+        }
+        failure {
+            echo 'Build or deployment failed!'
         }
     }
 }
