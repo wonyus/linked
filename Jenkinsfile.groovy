7182af52fd849cecad3b74b2573168d3d8e28f96
WonYus <lucy_oliv@outlook.com>
2024-05-04 15:59:19 +0700

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,60 +1,20 @@
 pipeline {
-    agent none
-
+    agent any
+    tools {
+        // a bit ugly because there is no `@Symbol` annotation for the DockerTool
+        // see the discussion about this in PR 77 and PR 52:
+        // https://github.com/jenkinsci/docker-commons-plugin/pull/77#discussion_r280910822
+        // https://github.com/jenkinsci/docker-commons-plugin/pull/52
+        'org.jenkinsci.plugins.docker.commons.tools.DockerTool' '18.09'
+    }
     environment {
-        DOCKER_IMAGE_NAME = 'wonyus/linked'
-        DOCKER_REGISTRY_CREDENTIALS = 'docker-credential'
+        DOCKER_CERT_PATH = credentials('id-for-a-docker-cred')
     }
-
     stages {
-        stage('Checkout') {
-            agent any
+        stage('foo') {
             steps {
-                checkout scm
+                sh 'docker version' // DOCKER_CERT_PATH is automatically picked up by the Docker client
             }
-        }
-
-        stage('Build Docker Image') {
-            agent {
-                docker {
-                    image 'node:18-alpine'
-                }
-            }
-            steps {
-                script {
-                    app = docker.build("${DOCKER_IMAGE_NAME}")
-                }
-            }
-        }
-
-        stage('Push Docker Image') {
-            agent any
-            steps {
-                script {
-                    docker.withRegistry('https://registry.hub.docker.com', "${DOCKER_REGISTRY_CREDENTIALS}") {
-                        app.push("${env.BUILD_NUMBER}")
-                        app.push('latest')
-                    }
-                }
-            }
-        }
-
-        stage('Deploy') {
-            agent any
-            steps {
-                // Add your deployment steps here
-                // For example:
-                sh 'pwd'
-            }
-        }
-    }
-
-    post {
-        success {
-            echo 'Build and deployment successful!'
-        }
-        failure {
-            echo 'Build or deployment failed!'
         }
     }
 }
