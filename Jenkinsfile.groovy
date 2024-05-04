78018501fe0c9297fa76ca1ab443e7065c749067
WonYus <lucy_oliv@outlook.com>
2024-05-04 16:08:58 +0700

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,7 @@
 pipeline {
     agent any
     tools {
-        // a bit ugly because there is no `@Symbol` annotation for the DockerTool
-        // see the discussion about this in PR 77 and PR 52:
-        // https://github.com/jenkinsci/docker-commons-plugin/pull/77#discussion_r280910822
-        // https://github.com/jenkinsci/docker-commons-plugin/pull/52
-        'org.jenkinsci.plugins.docker.commons.tools.DockerTool' '18.09'
+        DockerTool 'default'
     }
     environment {
         DOCKER_CERT_PATH = credentials('id-for-a-docker-cred')
