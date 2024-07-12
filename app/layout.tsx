9cda5c6823fd071efa9d0b26a5e927b6e8da107f
WonYus <lucy_oliv@outlook.com>
2024-07-13 04:13:54 +0700

    tag version 1.0.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import AuthProvider from "./context/AuthProvider";
 import NotifyStackProvider from "./context/NotifyStackProvider";
 import LocalizeDate from "./LocalizeDate";
 import ThemeApp from "./theme";
+import Footer from "./Footer";
 
 export const metadata: Metadata = {
   title: "Linked",
@@ -22,7 +23,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <AuthProvider>
               <body>
                 <Navbar />
-                <main style={{ margin: 20 }}>{children}</main>
+                <main style={{ margin: 20, minHeight: "90vh" }}>{children}</main>
+                <Footer />
               </body>
             </AuthProvider>
           </ThemeApp>
