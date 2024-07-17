ad45f416daf94dab8ee3256cf1986ddf8c2359a1
Miya784 <9burhanjehloh@gmail.com>
2024-07-17 17:03:33 +0700

    successfully open cam

---
+++ app/ai/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useRef, useState, useCallback } from "react";
+import Webcam from "react-webcam";
+import "./styles.css";
+
+const videoConstraints = {
+  width: 720,
+  height: 360,
+  facingMode: "user"
+};
+
+
+export default function Ai () {
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+    }
+  }, [webcamRef]);
+
+  return (
+    <>
+    <header>
+      <h1>camera app</h1>
+    </header>
+    {isCaptureEnable || (
+      <button onClick={() => setCaptureEnable(true)}>start</button>
+    )}
+    {isCaptureEnable && (
+      <>
+        <div>
+          <button onClick={() => setCaptureEnable(false)}>end </button>
+        </div>
+        <div>
+          <Webcam
+            audio={false}
+            width={540}
+            height={360}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+        </div>
+        <button onClick={capture}>capture</button>
+      </>
+    )}
+    {url && (
+      <>
+        <div>
+          <button
+            onClick={() => {
+              setUrl(null);
+            }}
+          >
+            delete
+          </button>
+        </div>
+        <div>
+          <img src={url} alt="Screenshot" />
+        </div>
+      </>
+    )}
+  </>
+  );
+}
