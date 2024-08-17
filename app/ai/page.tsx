f61465eb66e34f47e00148fa508270a6c285b156
WonYus <lucy_oliv@outlook.com>
2024-08-17 19:00:17 +0700

    fix ai page

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import ButtonAI from "@Components/InputAI/ButtonAI";
-import InputText from "@Components/InputAI/InputText";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import Image from "next/image";
 import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
@@ -15,26 +18,24 @@ const videoConstraints = {
 export default function Ai() {
   const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
   const webcamRef = useRef<Webcam>(null);
-  const [url, setUrl] = useState<string | null>(null);
+  const [imageString, setImageString] = useState<string | null>(null);
   const [messageResult, setMessageResult] = useState<string | null>(null);
   const [messageAsk, setMessageAsk] = useState<string>("");
 
   const capture = useCallback(() => {
-    const imageSrc = webcamRef.current?.getScreenshot();
+    const imageSrc = webcamRef.current?.getScreenshot({ width: 540, height: 360 });
     if (imageSrc) {
-      setUrl(imageSrc);
+      setImageString(imageSrc);
       GenerateMessage(imageSrc);
     }
   }, [webcamRef, messageAsk]);
 
   const GenerateMessage = async (imageSrc: string) => {
     try {
-      const { GoogleGenerativeAI } = require("@google/generative-ai");
-      const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+      const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY ?? "");
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
       const prompt = messageAsk || "What is in this photo?";
-
       const result = await model.generateContent([
         prompt,
         {
@@ -54,46 +55,53 @@ export default function Ai() {
 
   return (
     <>
-      <InputText label="Generate" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessageAsk(e.target.value)} />
-      <header>
-        <h1>Camera App</h1>
-      </header>
-      {!isCaptureEnable ? (
-        <ButtonAI label="Start" onClick={() => setCaptureEnable(true)} />
-      ) : (
-        <>
-          <div>
-            <ButtonAI label="End" onClick={() => setCaptureEnable(false)} />
-          </div>
-          <div>
-            <Webcam
-              audio={false}
-              width={540}
-              height={360}
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              videoConstraints={videoConstraints}
-            />
-          </div>
-          <ButtonAI label="Capture" onClick={capture} />
-        </>
-      )}
-      {url && (
-        <>
-          <div>
-            <ButtonAI label="Delete" onClick={() => setUrl(null)} />
-          </div>
-          <div>
-            <Image src={url} alt="capture" width={540} height={360} />
-          </div>
-          {messageResult && (
-            <div>
-              <h2>Generated Message</h2>
-              <p>{messageResult}</p>
-            </div>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Button variant="outlined" onClick={() => setCaptureEnable((state) => !state)}>
+            {isCaptureEnable ? "Close" : "Open Camera"}
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          {imageString ? (
+            <Image src={imageString} alt="capture" width={540} height={360} />
+          ) : (
+            <>
+              {isCaptureEnable && (
+                <Webcam audio={false} width={540} height={360} ref={webcamRef} screenshotFormat="image/jpeg" videoConstraints={videoConstraints} />
+              )}
+            </>
           )}
-        </>
-      )}
+        </Grid>
+        {imageString != null && (
+          <Grid item xs={12}>
+            <Button variant="outlined" onClick={() => setImageString(null)}>
+              {"Re Capture"}
+            </Button>
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          <TextField
+            type="text"
+            label="Command"
+            name="Command"
+            fullWidth
+            margin="normal"
+            value={messageAsk}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessageAsk(e.target.value)}
+          />
+          {isCaptureEnable && !imageString && (
+            <Button variant="outlined" onClick={capture}>
+              {"Send"}
+            </Button>
+          )}
+          {messageResult && (
+            <>
+              <Typography variant="h5">{"Result"}</Typography>
+              <Typography variant="body2">{messageResult}</Typography>
+            </>
+          )}
+        </Grid>
+      </Grid>
     </>
   );
 }
